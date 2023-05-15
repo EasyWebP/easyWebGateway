4050551618941fd0c7b4f81efcb6243f00da1f72
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-15 14:39:31 +0900

    first commit

---
+++ src/main/java/easyweb/easywebgateway/EasyWebGatewayApplication.java
@@ -0,0 +1,13 @@
+package easyweb.easywebgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWebGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/easyweb/easywebgateway/EasyWebGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package easyweb.easywebgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWebGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
