4fff90ba53775da466042149b1a280133aa26100
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-05-18 10:04:16 +0900

    [ADD] Http URI 로깅 추가

--- src/main/java/easyweb/easywebgateway/EasyWebGatewayApplication.java
+++ src/main/java/easyweb/easywebgateway/EasyWebGatewayApplication.java
@@ -1,8 +1,11 @@
 package easyweb.easywebgateway;
 
+import easyweb.easywebgateway.component.RequestLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,14 @@ public class EasyWebGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyWebGatewayApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter() {
+        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestLoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 
 }
